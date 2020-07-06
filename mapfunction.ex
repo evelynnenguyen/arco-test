f4736f7ae192574b191d488b51499a2f161f1d74
Nguyen Nguyen <nguyennguyen6395@gmail.com>
2020-07-06 15:11:49 +1200

    updated

--- mapfunction.ex
+++ mapfunction.ex
@@ -1,18 +1,23 @@
 defmodule Arco do
   # Define your map function here
-  def map(a_list, a_fucntion) do
-    if length(a_list) >= 2 do
-      IO.puts("list length is larger than 2")
-    end
-    else
-      IO.puts("list length is smaller than 2")
-    end
+  def map(a_list, a_function) when length(a_list) <= 1 do
+      IO.puts("list length is smaller or equal to 1")
+      [head | tail] = a_list
+      IO.puts(head)
+      head = a_function.(head)
+      IO.puts(head)
+      IO.puts(a_list)
+  end
+  def map(a_list, a_function) do
+      IO.puts("list length is larger or equal to 2")
+      [head | tail] = a_list
+      map(tail, a_function)
   end
 end
 
 import ExUnit.Assertions
 
-assert Arco.map([1], &(&1 + 1)) ==[2]
+assert Arco.map([1], &(&1 + 2)) ==[2]
 assert Arco.map([1, 2, 3], &(&1 + 1)) ==[2, 3, 4]
 assert Arco.map([1, 2, 3], &(&1 * 2)) == [2, 4, 6]
 assert Arco.map(["a", "b", "c"], &String.upcase/1) == ["A", "B", "C"]
