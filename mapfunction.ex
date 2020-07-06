6386cea9778ca3f15f1f804f310dd34156a9d157
Nguyen Nguyen <nguyennguyen6395@gmail.com>
2020-07-06 16:08:11 +1200

    done functionality wise

--- mapfunction.ex
+++ mapfunction.ex
@@ -2,15 +2,16 @@ defmodule Arco do
   # Define your map function here
   def map(a_list, a_function) when length(a_list) <= 1 do
       IO.puts("list length is smaller or equal to 1")
-      IO.puts(a_function.(List.first(a_list)))
       a_list = List.replace_at(a_list, 0, a_function.(List.first(a_list)))
       IO.inspect a_list, charlists: :as_lists
   end
   def map(a_list, a_function) do
       IO.puts("list length is larger or equal to 2")
       IO.inspect a_list, charlists: :as_lists
+      a_list = List.replace_at(a_list, 0, a_function.(List.first(a_list)))
       [head | tail] = a_list
-      map(tail, a_function)
+      IO.inspect a_list, charlists: :as_lists
+      a_list = Enum.take(a_list, 1) ++ map(tail, a_function)
   end
 end
 
